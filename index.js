901ff60301af39d33454142af530f29724860416
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 04:49:33 -0400

    Add function changeCompletely

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var evens = [0, 2, 4, 6, 8, 10];
+var animals = ["dog", "fish", "cat"];
 
 function doToEvens(callback) {
   evens.forEach(callback)
@@ -7,3 +8,10 @@ function doToEvens(callback) {
 function doToElementsInArray(array, callback) {
   array.forEach(callback)
 }
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+doToElementsInArray(animals, changeCompletely);
+console.log(animals);
