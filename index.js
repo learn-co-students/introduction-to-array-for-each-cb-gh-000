8ce84ab80345938a396819caa64cc576d47d823c
Victor Alagwu <victoralagwu@gmail.com>
2017-12-05 14:37:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray(){
-
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely(){
-  
+function changeCompletely(element,index,array){
+  array[index] = '!!';
 }
+var name = ['Victor',' Henry', 'Johnpaul','Ruth','Benita'];
+
+doToElementsInArray(name, changeCompletely);
+console.log(name);
